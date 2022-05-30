0a420d6138e1c69f0edd6904f99a20864e3ea164
Evan Garcia <evanag116@yahoo.com>
2022-05-30 09:20:56 -0500

    completed in py

--- python/pad_array.py
+++ python/pad_array.py
@@ -1,2 +1,10 @@
-#REMEMBER TO PSEUDOCODE
 def pad(array, min_size, value = None):
+    x = min_size - len(array)
+
+    while x > 0:
+        array.append(value)
+        x -= 1
+    return array
+
+
+# print(pad([1,2,3], 5) == [1,2,3,None,None])
--- python/pad_array_spec.py
+++ python/pad_array_spec.py
@@ -1,1 +1,6 @@
-# Write unit tests!
+from pad_array import pad
+
+
+print(pad([1,2,3], 5) == [1,2,3,None,None])
+print(pad([1,2,3], 5, 'apple') == [1,2,3,'apple', 'apple'])
+print(pad([1,2,3,4,5], 10, 10) == [1,2,3,4,5,10,10,10,10,10])
