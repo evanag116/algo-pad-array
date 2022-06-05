09717a7165f799f3eec6a8df1298d28977982ad7
Evan Garcia <evanag116@yahoo.com>
2022-06-05 18:38:00 -0500

    completed in js

--- js/padArray.js
+++ js/padArray.js
@@ -1,4 +1,14 @@
-// REMEMBER TO PSEUDOCODE
-const pad = (array, minSize, value=null) => {
+exports.padArray = function (arr, minSize, value=undefined) {
+
+    if (arr.length < minSize) {
+        let answer = arr
+        for (let i = 0; minSize > arr.length; i++) {
+            answer.push(value)
+        }
+        return answer
+    } else {
+        return arr
+    }  
 
 }
+console.log(this.padArray([1,2,3],5))
